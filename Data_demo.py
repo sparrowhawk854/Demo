11e2783bfc8005be70fd00d2625c7449e63779b9
sparrowhawk854 <130519849+sparrowhawk854@users.noreply.github.com>
2023-04-12 15:00:12 +1200

    Update Data_demo.py

--- Data_demo.py
+++ Data_demo.py
@@ -1,12 +1,46 @@
 import streamlit as st
+import snowflake.connector
+import pandas as pd
+import altair as alt
+
+# Set up Snowflake connection
+conn = snowflake.connector.connect(
+    user='<Hamburger>',
+    password='<NumNumNum99>',
+    account='<australia-east.azure/pt11496>',
+    warehouse='<snowflake>',
+    database='<snowflake_sample_data>',
+    schema='<TPCH_SF1>'
+
 
 # Set the title of the app
-st.title("Data Talk")
+st.title("Hamburgler")
 
 # Add a text input for the user to search
 search_term = st.text_input("Enter a search term")
 
-# Use the search term to perform some action, such as querying a database or API
+# Use the search term to perform a SQL query on the Snowflake database
 if search_term:
-    # Perform some action with the search term
-    st.write("You searched for:", search_term)
+    # Perform the SQL query
+    cursor = conn.cursor()
+    query = f"SELECT * FROM CUSTOMER WHERE C_NAME LIKE '%{search_term}%'"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    
+    # Display the results as a table
+    if results:
+        df = pd.DataFrame(results, columns=['C_MKTSEGMENT', 'C_PHONE', 'C_ACCTBAL', 'C_ADDRESS', 'C_NATIONKEY', 'C_CUSTKEY', 'C_NAME', 'C_COMMENT'])
+        st.write(df)
+        
+        # Create a bar chart of the results
+        chart = alt.Chart(df).mark_bar().encode(
+            x='C_NAME',
+            y='C_ACCTBAL'
+        ).properties(
+            width=600,
+            height=400
+        )
+        st.altair_chart(chart, use_container_width=True)
+        
+    else:
+        st.write("No results found.")
