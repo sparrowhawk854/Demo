ecdce0ee45fed03f0fd4249a9e9430651c96dba7
sparrowhawk854 <130519849+sparrowhawk854@users.noreply.github.com>
2023-04-12 14:48:40 +1200

    Update Data_demo.py

--- Data_demo.py
+++ Data_demo.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+# Set the title of the app
+st.title("My Streamlit App")
+
+# Add a text input for the user to search
+search_term = st.text_input("Enter a search term")
+
+# Use the search term to perform some action, such as querying a database or API
+if search_term:
+    # Perform some action with the search term
+    st.write("You searched for:", search_term)
